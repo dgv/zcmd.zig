48f4038e66124253f2805091c4b5fcc9c0dcc731
Daniel Gomes Vargas <dgv@fastmail.com>
2024-06-10 17:06:41 -0300

    zig std.ChildProcess was renamed to std.process.Child

--- tests/run_child_test.zig
+++ tests/run_child_test.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 test "stop test" {
-    const rr = try std.ChildProcess.run(.{
+    const rr = try std.process.Child.run(.{
         .allocator = @import("std").testing.allocator,
         .argv = &[_][]const u8{"./exit_stop"},
     });
